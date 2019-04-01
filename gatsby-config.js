a09403ece034138ff47d2bee02d07b938d51273c
Tom Dyson <tom@torchbox.com>
2019-04-01 17:57:24 +0100

    Initial commit from gatsby: (https://github.com/niklasmtj/gatsby-starter-julia.git)

---
+++ gatsby-config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  siteMetadata: {
+    title: `Julia Doe`,
+    description: `A minimal blog starter built with Gatsbyjs. The needes Gatsby files are included.`,
+    author: `@niklasmtj`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/content`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-remark-reading-time`,
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // 'gatsby-plugin-offline',
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import {css} from "@emotion/core"
+
+import LandingBio from "../components/landing-bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <LandingBio />
+  </Layout>
+)
+
+export default IndexPage
